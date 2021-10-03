541157ca3b8cad085f9a7422d1327dcc06609426
Kyle Daley <guymella@hotmail.com>
2021-10-03 16:55:58 -0600

    Bridge connected. lights controlled

---
+++ API_Adapter.h
@@ -0,0 +1,179 @@
+//
+// Created by k on 10/3/21.
+//
+
+#ifndef HUE_CONSOLE_API_ADAPTER_H
+#define HUE_CONSOLE_API_ADAPTER_H
+
+#include "httplib.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+std::string Append_Path(const std::string& root, const std::string& path){
+    std::stringstream r;
+    r << root << "/" << path;
+    return r.str();
+}
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+std::string Apply_Path_Variables(const std::string& path, const json& parms){
+    if(parms.contains("pathvars")){
+        std::string new_path = path;
+        json v = parms["pathvars"];
+        for (json::iterator it = v.begin(); it != v.end(); ++it) {
+            replace(new_path,it.key(),it.value());
+        }
+        return new_path;
+    }
+    return path;
+}
+
+class API_Adapter;
+
+class iAPI {
+public:
+    virtual httplib::Result Call(API_Adapter &api, json& parms) = 0;
+    inline virtual httplib::Result Call(API_Adapter &api) {json j; return Call(api,j);};
+};
+
+class API_Adapter {
+public:
+    //API_Adapter(std::string host, int port, std::string root, char* Init_Route = nullptr, char* Init_Body = nullptr, char* Init_Body_Type = nullptr, std::string Prompt = "");
+    API_Adapter(std::string host, int port, std::string root);
+    API_Adapter(std::string host, int port, std::string root, std::function<void(API_Adapter&)>& handshake);
+
+    void Handshake(std::function<void(API_Adapter&)>& handshake);
+    void Append_Root(std::string route);
+    httplib::Client& Client();
+    const std::string& Root() const;
+
+    void New_Get(std::string Name, std::string Path);
+    void New_Put(std::string Name, std::string Path);
+    void New_Post(std::string Name, std::string Path);
+    void New_Delete(std::string Name, std::string Path);
+
+    httplib::Result Call(const std::string& method) {return methods[method]->Call(*this);};
+    httplib::Result Call(const std::string& method,json& parms) {return methods[method]->Call(*this,parms);};;
+
+
+private:
+    httplib::Client cli;
+    std::string _api_root;
+    std::string session_ID = "";
+    std::map<std::string, iAPI *> methods;
+
+};
+
+
+
+class API_Get : iAPI {
+public:
+    API_Get (std::string path) : _path(path) {};
+    httplib::Result Call(API_Adapter &api, json& parms) override;
+private:
+    std::string _path;
+};
+
+httplib::Result API_Get::Call(API_Adapter &api, json &parms) {
+    return api.Client().Get(Append_Path(api.Root(),Apply_Path_Variables(_path,parms)).c_str());
+}
+
+
+class API_Put : iAPI {
+public:
+    API_Put (std::string path): _path(path) {};
+    httplib::Result Call(API_Adapter &api, json& parms) override;
+private:
+    std::string _path;
+};
+
+httplib::Result API_Put::Call(API_Adapter &api, json &parms) {
+    return api.Client().Put(Append_Path(api.Root(),Apply_Path_Variables(_path,parms)).c_str()
+                            ,(char*)(parms["body"].dump().c_str())
+                            ,"application/json");
+}
+
+class API_Post : iAPI {
+public:
+    API_Post (std::string path): _path(path) {};
+    httplib::Result Call(API_Adapter &api, json& parms) override;
+private:
+    std::string _path;
+};
+
+httplib::Result API_Post::Call(API_Adapter &api, json &parms) {
+    //TODO::
+    return api.Client().Get(Append_Path(api.Root(),Apply_Path_Variables(_path,parms)).c_str());
+}
+
+class API_Delete : iAPI {
+public:
+    API_Delete (std::string path): _path(path) {};
+    httplib::Result Call(API_Adapter &api, json& parms) override;
+private:
+    std::string _path;
+};
+
+httplib::Result API_Delete::Call(API_Adapter &api, json &parms) {
+    //TODO::
+    return api.Client().Get(Append_Path(api.Root(),Apply_Path_Variables(_path,parms)).c_str());
+}
+
+API_Adapter::API_Adapter(std::string host
+                         , int port
+                         , std::string root)
+                         : cli(host, port)
+                         , _api_root(root) {
+}
+
+API_Adapter::API_Adapter(std::string host
+                         , int port
+                         , std::string root
+                         , std::function<void(API_Adapter &)>& handshake)
+                            : cli(host, port)
+                            , _api_root(root) {
+    Handshake(handshake);
+}
+
+void API_Adapter::Append_Root(std::string path) {
+    _api_root = Append_Path(_api_root,path);
+}
+
+httplib::Client& API_Adapter::Client() {
+    return cli;
+}
+
+const std::string &API_Adapter::Root() const {
+    return _api_root;
+}
+
+void API_Adapter::Handshake(std::function<void(API_Adapter&)>& handshake) {
+    handshake(*this);
+}
+
+void API_Adapter::New_Get(std::string Name, std::string Path) {
+    methods[Name] = (iAPI*) new API_Get(Path);
+}
+
+void API_Adapter::New_Put(std::string Name, std::string Path) {
+    methods[Name] = (iAPI*) new API_Put(Path);
+}
+
+void API_Adapter::New_Post(std::string Name, std::string Path) {
+    methods[Name] = (iAPI*) new API_Post(Path);
+}
+
+void API_Adapter::New_Delete(std::string Name, std::string Path) {
+    methods[Name] = (iAPI*) new API_Delete(Path);
+}
+
+
+#endif //HUE_CONSOLE_API_ADAPTER_H
--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 #include "httplib.h"
 #include "json.hpp"
 
-using namespace nlohmann;
+#include "API_Adapter.h"
+
+using json = nlohmann::json;
 
 void configure_parser(cli::Parser& parser) {
     parser.set_optional<std::string>("v", "virtual_bridge", "192.168.0.0", "IP Address of virtual bridge.");
+    parser.set_optional<std::string>("b", "bridge", "10.0.0.0", "IP Address of bridge.");
+
     parser.set_optional<int>("p", "port", 80, "Port of bridge.");
+    parser.set_optional<std::string>("u", "username", "", "Authorized username on the bridge.");
 }
 
 int main(int argc, char** argv) {
@@ -17,10 +22,22 @@ int main(int argc, char** argv) {
     configure_parser(parser);
     parser.run_and_exit_if_error();
 
+
+
     auto URL = parser.get<std::string>("v");
+
     auto port = parser.get<int>("p");
-    std::cout << "Virtual Bridge API URL: " << URL << std::endl;
-    std::cout << "Port Number: " << port << std::endl << std::endl;
+    auto username = parser.get<std::string>("u");
+    if (parser.doesArgumentExist("b","bridge")){
+        URL = parser.get<std::string>("b");
+    } else std::cout << "Virtual ";
+    std::cout << "Bridge API URL: " << URL << std::endl;
+    std::cout << "Port Number: " << port << std::endl;
+    if (parser.doesArgumentExist("u","username")){
+        std::cout << "Username: " << username << std::endl;
+    }
+    std::cout << std::endl;
+
 
     //Test http API
 
@@ -35,6 +52,123 @@ int main(int argc, char** argv) {
     json j = json::parse(res->body);
     std::cout << "Test json"<< std::endl;
     std::cout << j.dump(4) << std::endl << std::endl;
+
+
+
+
+    //Init API
+
+    API_Adapter api(URL,port,"/api");
+
+    if(parser.doesArgumentExist("u","username")){//username specified. no handshake needed
+        api.Append_Root(username);
+    }else{ //no username specified. handshake to get one.
+        if(parser.doesArgumentExist("v","virtual_bridge")){//handshake for virtual bridge
+
+            //handshake callback
+            std::function<void(API_Adapter &)> handshake = [](API_Adapter& a){
+                //build body
+                json hb = {{"devicetype" , "test user"},{"username","newuser"}};
+
+                //prompt for link button
+                std::cout << "Please Press The Link Button On your Bridge. Then press ""ENTER"" to continue..." <<std::endl;
+                std::cin.get();
+
+                //post
+                auto res = a.Client().Post((char*)(a.Root().c_str()),(char*)(hb.dump().c_str()),"application/json");
+
+                if(res.error() == httplib::Error::Success){//bridge found
+                    json j = json::parse(res->body);
+                    if (j[0].contains("success")){//username created
+                        //add username to api route
+                        a.Append_Root(j[0]["success"]["username"]);
+                        std::cout << "Handshake Successful" << std::endl << std::endl ;
+                    } else std::cout << "Handshake Failed. Make sure you press the Link Button on the bridge." << std::endl << std::endl ;
+                } else std::cout << "Bridge Not Found. Check IP address and port number." << std::endl << std::endl ;
+
+            };
+            //make handshake
+            api.Handshake(handshake);
+        } else {//else handshake for real bridge
+            //handshake callback
+            std::function<void(API_Adapter &)> handshake = [](API_Adapter& a){
+                //build body {"devicetype":"my_hue_app#iphone peter"}
+                json hb = {{"devicetype" , "my_hue_app#console"}};
+
+                //prompt for link button
+                std::cout << "Please Press The Link Button On your Bridge. Then press ""ENTER"" to continue..." <<std::endl;
+                std::cin.get();
+
+                //post
+                auto res = a.Client().Post((char*)(a.Root().c_str())
+                                            ,(char*)(hb.dump().c_str())
+                                            ,"application/json");
+
+                if(res.error() == httplib::Error::Success){//bridge found
+                    json j = json::parse(res->body);
+                    if (j[0].contains("success")){//username created
+                        //add username to api route
+                        a.Append_Root(j[0]["success"]["username"]);
+                        std::cout << "Handshake Successful" << std::endl << std::endl ;
+                    } else std::cout << "Handshake Failed. Make sure you press the Link Button on the bridge." << std::endl << std::endl ;
+                } else std::cout << "Bridge Not Found. Check IP address and port number." << std::endl << std::endl ;
+
+            };
+            //make handshake
+            api.Handshake(handshake);
+        }
+    }
+
+    std::cout <<"api root: " << api.Root()<< std::endl << std::endl;
+
+    //API connected
+    //setup interfaces
+
+    api.New_Get("Get_Lights","lights");
+    api.New_Put("Set_Light_State", "lights/id/state");
+
+    //Get Lights
+
+    auto gl_res = api.Call("Get_Lights");
+    if(gl_res.error() == httplib::Error::Success){
+        json lights = json::parse(gl_res->body);
+        std::cout << lights.dump(4) << std::endl << std::endl;
+
+    } else std::cout << "bad call gl"<< std::endl << std::endl;
+
+    //set Light state
+
+    json sl = {{"pathvars", {{"id" , "9"}}} , {"body" , {{"bri", 254}}}};
+    auto sl_res = api.Call("Set_Light_State",sl);
+    if(sl_res.error() == httplib::Error::Success){
+        json lights = json::parse(sl_res->body);
+        std::cout << lights.dump(4) << std::endl << std::endl;
+
+    } else std::cout << "bad call sl"<< std::endl << std::endl;
+
+    for (uint8_t i = 0; i< 4 ; i++){
+        json sl2 = {{"pathvars", {{"id" , "28"}}} , {"body" , {{"bri", 1}}}};
+        auto sl2_res = api.Call("Set_Light_State",sl2);
+        if(sl2_res.error() == httplib::Error::Success){
+            json lights = json::parse(sl2_res->body);
+            std::cout << lights.dump(4) << std::endl << std::endl;
+
+        } else std::cout << "bad call sl2"<< std::endl << std::endl;
+
+        sleep(1);
+
+        json sl2o = {{"pathvars", {{"id" , "28"}}} , {"body" , {{"bri", 254}}}};
+        auto sl2o_res = api.Call("Set_Light_State",sl2o);
+        if(sl2o_res.error() == httplib::Error::Success){
+            json lights = json::parse(sl2o_res->body);
+            std::cout << lights.dump(4) << std::endl << std::endl;
+
+        } else std::cout << "bad call sl2"<< std::endl << std::endl;
+        sleep(1);
+    }
+
+
+    //Get Light States
 
     return 0;
 }
